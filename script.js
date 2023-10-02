6a3cc3526ea17f651a22e87b3f8a5546b5b235d6
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-10-02 22:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const priceElements = document.querySelectorAll('.price');
+  let totalPrice = 0;
+
+  // Loop through the price elements and calculate the total price
+  priceElements.forEach((priceElement) => {
+    const price = parseFloat(priceElement.textContent);
+    if (!isNaN(price)) {
+      totalPrice += price;
+    }
+  });
+
+  // Display the total price
+  alert(`Total Price: Rs. ${totalPrice.toFixed(2)}`);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
